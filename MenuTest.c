09710e581b3555821baf06551418a47082e98cc1
Ben <benjamin@thomasnetwork.net>
2011-11-18 22:26:18 -0500

    Menu test bug discovered
    bug, screen does not refresh the right way

--- MenuTest.c
+++ MenuTest.c
@@ -1,10 +1,59 @@
 int MenuItem = 1;
-int MenuSwitch = 1;
-int Enter;
+int MenuNum = 1;
+int Enter = 0;
 int Exit;
 void Menu()
 {
+    switch (MenuNum)
+    {
+        case 1:
+            switch (MenuItem)
+        {
+            case 1:
+                nxtDisplayString(1,"<Test Code>");
+                nxtDisplayString(2,"Edit Code");
+                nxtDisplayString(3,"Exit");
+                break;
+            case 2:
+                nxtDisplayString(1,"Test Code");
+                nxtDisplayString(2,"<Edit Code>");
+                nxtDisplayString(3,"Exit");
+                break;
+            case 3:
+                nxtDisplayString(1,"Test Code");
+                nxtDisplayString(2,"Edit Code");
+                nxtDisplayString(3,"<Exit>");
+                break;
+            default:
+                nxtDisplayString(1,"<Test Code>");
+                nxtDisplayString(2,"Edit Code");
+                nxtDisplayString(3,"Exit");
+                break;
 
+            }
+            if (Enter == 1)
+            {
+              Enter = 0;
+                switch (MenuItem)
+                {
+                    case 1:
+                        MenuNum = 2;
+                        MenuItem = 1;
+                        break;
+                }
+            }
+            break;
+        case 2:
+            switch (MenuItem)
+        {
+                case 1:
+                    nxtDisplayString(1,"Run code Menu");
+                    break;
+
+                default:
+                    break;
+            }
+    }
 }
 
 
@@ -26,10 +75,10 @@ task main()
 	  {
 	  	case kLeftButton:	 --MenuItem;    		      	  break;
 			case kRightButton: ++MenuItem;    	 	          break;
-			case kEnterButton: Enter = 1;   MenuItem = 1;   break;
-			case kExitButton:	 Exit = 1;     	  		        break;
+			case kEnterButton: Enter = 1;                   break;
+			case kExitButton:	 Exit = 1;                      break;
 		}
-		nxtDisplayString(0,"%i",MenuItem);
+		nxtDisplayString(0,"MI%i MN%i",MenuItem ,MenuNum);
 		Menu();
 
 	  while (true)
